58e7134f858ee1365814dec4d9d2eac72cee35e1
Ray Chen <ray@hyperchain.cn>
2017-01-26 17:25:06 +0800

    [FAB-1508] Update bddtests Python dependencies
    The BDD tests now require the 'ecdsa' and 'python-slugify' package as
    its dependencies.  Add them to the devenv setup scripts.
    
    The latest grpcio and grpcio-tools package are also required.  Update
    them, too.
    
    The SHA3 package we needed is 'pysha3', not 'sha3'.  Fix it.
    
    Update the documentation.
    
    Change-Id: Icd6dec3c54fb4764d133dda0cc7640aeb3e49bf9
    Signed-off-by: Ray Chen <ray@hyperchain.cn>

--- devenv/setupUbuntuOnPPC64le.sh
+++ devenv/setupUbuntuOnPPC64le.sh
@@ -85,5 +85,5 @@ cd - ; rm -rf /tmp/rocksdb
 apt-get -y install python-pip
 pip install --upgrade pip
 pip install behave nose docker-compose
-
-pip install -I flask==0.10.1 python-dateutil==2.2 pytz==2014.3 pyyaml==3.10 couchdb==1.0 flask-cors==2.0.1 requests==2.4.3 grpcio==0.13.1 pyOpenSSL==16.2.0 sha3==0.2.1
+pip install --upgrade grpcio-tools
+pip install -I flask==0.10.1 python-dateutil==2.2 pytz==2014.3 pyyaml==3.10 couchdb==1.0 flask-cors==2.0.1 requests==2.4.3 pyopenssl==16.2.0 pysha3==1.0b1 ecdsa==0.13 python-slugify==1.2.1
